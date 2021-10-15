47db55223df3c56a71ba56702b65f7fd2fca69cc
James Pflaging <pflagingjames@gmail.com>
2021-10-15 16:22:59 -0700

    one

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer () {
+    return (
+        <footer>
+            <h3>This a footeR!</h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MainContent () {
+    return (
+        <main>
+            asd;flkas;ldfkha;s
+        </main>
+    )
+}
+
+export default MainContent
